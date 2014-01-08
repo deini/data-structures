d5452a1841de04c307177093a68fcdb01b3d4acd
Daniel Almaguer <danielalmaguer@gmail.com>
2014-01-08 12:45:16 -0800

    Implemented .enqueue to not need shifting all the keys

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,27 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var size = 0;
   // Implement the methods below
 
   instance.enqueue = function(value){
+    storage[size++] = value;
   };
 
   instance.dequeue = function(){
+    if(Object.keys(storage).length) {
+      var key = Object.keys(storage)[0];
+      var front = storage[key];
+      delete storage[key];
+      return front;
+    } else {
+      size = 0;
+      return undefined;
+    }
   };
 
   instance.size = function(){
+    return Object.keys(storage).length;
   };
 
   return instance;
