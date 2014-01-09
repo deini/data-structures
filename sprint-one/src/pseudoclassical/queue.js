14dcbc284116f7baeaf5655305c900cb010c6af0
chughes87 <c.hughes87@gmail.com>
2014-01-08 17:31:18 -0800

    Copied from prototypal

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,3 +1,32 @@
 var Queue = function() {
   // Hey! Copy your code from src/prototypal/queue.js and paste it here
 };
+
+var makeQueue = function(){
+  var instance = Object.create(queueMethods);
+
+  instance.storage = {};
+  instance.thesize = 0;
+  instance.first = 0;
+  
+  return instance;
+};
+
+var queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+  this.storage[this.thesize+this.first] = value;
+  this.thesize++;
+};
+
+queueMethods.dequeue = function() {
+  this.thesize = this.thesize && --this.thesize;
+  var result = this.storage[this.first];
+  delete this.storage[this.first];
+  this.first++;
+  return result;
+};
+
+queueMethods.size = function() {
+  return this.thesize;
+}
