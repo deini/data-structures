73e6e46bcc80691686ec8b4307864f3ea3105c1e
chughes87 <c.hughes87@gmail.com>
2014-01-08 17:31:43 -0800

    Copied code from prototypal

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,3 +1,34 @@
 var Stack = function() {
   // Hey! Copy your code from src/prototypal/stack.js and paste it here
 };
+
+var makeStack = function(){
+  var instance = Object.create(stackMethods);
+
+  // Use an object with numeric keys to store values
+  instance.storage = {};
+  instance.thesize = 0;
+  
+  return instance;
+};
+
+var stackMethods = {};
+
+stackMethods.push = function(value) {
+  this.storage[this.thesize] = value;
+  this.thesize++;
+};
+
+stackMethods.pop = function() {
+  if (this.thesize) {
+    this.thesize--;
+    var result = this.storage[this.thesize];
+    delete this.storage[this.thesize];
+  }
+  return result;
+};
+
+stackMethods.size = function() {
+  return this.thesize;
+};
+
