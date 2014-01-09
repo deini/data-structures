93fcb11bad75e1dfea734530cd91eb90ef5984be
chughes87 <c.hughes87@gmail.com>
2014-01-08 16:37:41 -0800

    Copied the code from functional-shared

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,5 +1,32 @@
-var makeStack = function() {
-  // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+var makeStack = function(){
+  var instance = {};
+
+  // Use an object with numeric keys to store values
+  instance.storage = {};
+  instance.thesize = 0;
+  _.extend(instance, stackMethods);
+  
+  return instance;
 };
+
+var stackMethods = {};
+
+stackMethods.push = function(value) {
+  this.storage[this.thesize] = value;
+  this.thesize++;
+};
+
+stackMethods.pop = function() {
+  if (this.thesize) {
+    this.thesize--;
+    var result = this.storage[this.thesize];
+    delete this.storage[this.thesize];
+  }
+  return result;
+};
+
+stackMethods.size = function() {
+  return this.thesize;
+}
 
 var stackMethods = {};
