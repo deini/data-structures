665cc1c2499b9c93bebca25e5811c45590895796
chughes87 <c.hughes87@gmail.com>
2014-01-08 16:36:40 -0800

    Copied the stack code from functional-shared

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,5 +1,33 @@
-var makeQueue = function() {
-  // Hey! Copy your code from src/functional-shared/queue.js and paste it here
+var makeQueue = function(){
+  var instance = {};
+
+  instance.storage = {};
+  instance.thesize = 0;
+  instance.first = 0;
+  _.extend(instance, queueMethods);
+  
+  return instance;
 };
+
+var queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+  this.storage[this.thesize+this.first] = value;
+  this.thesize++;
+};
+
+queueMethods.dequeue = function() {
+  this.thesize = this.thesize && --this.thesize;
+  var result = this.storage[this.first];
+  delete this.storage[this.first];
+  this.first++;
+  return result;
+};
+
+queueMethods.size = function() {
+  return this.thesize;
+}
+
+
 
 var queueMethods = {};
