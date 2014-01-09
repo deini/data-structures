d4ad757502763ef628e1174ad24d50801725e37d
chughes87 <c.hughes87@gmail.com>
2014-01-09 10:18:40 -0800

    Implemented removeHead and contains

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,13 +4,37 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
-    list.tail = makeNode(value);
+    if(list.tail) {
+      list.tail.next = makeNode(value);
+      list.tail = list.tail.next;
+    } else {
+      list.tail = list.head = makeNode(value);
+    }
   };
 
   list.removeHead = function(){
+    if (list.head){
+      var result = list.head.value;
+      if (list.head === list.tail){
+        list.head = null;
+        list.tail = null;
+      } else {
+        list.head = list.head.next;
+      }
+      return result;
+    }
+    return undefined;
   };
 
   list.contains = function(target, node){
+    var current = list.head;
+    while(current) {
+      if (current.value === target){
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
   };
 
   return list;
