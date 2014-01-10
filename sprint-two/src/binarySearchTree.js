8a79f89c9f9f607238531c1db42901adcd4d018b
chughes87 <jeanette.pettibone@gmail.com>
2014-01-09 19:59:45 -0800

    Created binarySearchTree structure

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,2 +1,18 @@
-var makeBinarySearchTree = function(value){
+var BinarySearchTree = function(value){
+
+  this.left  = null;
+  this.right = null;
+
+};
+
+BinarySearchTree.prototype.insert = function() {
+
+};
+
+BinarySearchTree.prototype.contains = function() {
+
+};
+
+BinarySearchTree.prototype.depthFirstLog = function() {
+
 };
