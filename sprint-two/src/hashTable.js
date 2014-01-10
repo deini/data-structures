b636a0b59cdaf28892ee6ebad0f19dad8eb9a6cf
chughes87 <c.hughes87@gmail.com>
2014-01-10 15:32:16 -0800

    Implemented hashTable using an array of arrays of key-value pairs

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,16 +5,37 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(i, v);
+  if (Array.isArray(this._storage[i])) {
+    var arr = this._storage.get(i);
+    arr.push([k, v]);
+    this._storage.set(arr);
+  } else { 
+    this._storage.set(i, [[k, v]]);
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(i);
+  var arr = this._storage.get(i);
+  if(arr && arr.length > 0) {
+    for(var j = 0; j < arr.length; j++) {
+      if(arr[j][0] === k) {
+        return arr[j][1];
+      }
+    }
+  } else {
+    return null;
+  }
 };
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(i, null);
+  var bucket = this._storage.get(i);
+
+  for(var j = 0; j < bucket.length; j++) {
+    if(bucket[j][0] === k) {
+      bucket.splice(j, 1);
+    }
+  }
 };
 
