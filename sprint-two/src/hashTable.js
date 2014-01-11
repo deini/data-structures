3abc369bd109d3bb85c640caf55bd2db49a6b320
chughes87 <jeanette.pettibone@gmail.com>
2014-01-11 15:11:12 -0800

    Implemented reHash instead of doubleHash and halveHash; worked in each loops

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -8,50 +8,59 @@ HashTable.prototype.insert = function(k, v){
   if (++this._pairs >= (this._limit * 0.75)) {
     this.reHash(this._limit * 2);
   }  
-  var i = getIndexBelowMaxForKey(k, this._limit);
-  if (Array.isArray(this._storage.get(i))) {
-    var arr = this._storage.get(i);
 
-    var temp = this;
-    $.each(arr, function(index) {
-      if (this[0] === k) {
-        this[1] = v;
-        return temp._storage.set(i, arr);
+  var i = getIndexBelowMaxForKey(k, this._limit);
+
+  if (Array.isArray(this._storage.get(i))) {
+    var bucket = this._storage.get(i);
+    var hash = this;
+
+    each(bucket, function(item, index) {
+      if (item[0] === k) {
+        item[1] = v;
+        return hash._storage.set(i, bucket);
       }
     });
-    arr.push([k, v]);
-    this._storage.set(i, arr); 
-  } else { 
+
+    bucket.push([k, v]);
+    this._storage.set(i, bucket); 
+  } 
+  else { 
     this._storage.set(i, [[k, v]]);
   }
 }; 
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  var arr = this._storage.get(i);
-  if(arr) {
-    for(var j = 0; j < arr.length; j++) {
-      if(arr[j][0] === k) {
-        return arr[j][1];
-      }
-    }
-  } else {
+  var bucket = this._storage.get(i);
+
+  if(bucket) {
+    var found = null;
+    each(bucket, function(item, i) {
+      if (item[0] === k) {
+        found = item[1];
+      } 
+    });
+    return found;
+  } 
+  else {
     return null;
   }
 };
 
 HashTable.prototype.remove = function(k){
-  this._pairs--;
   var i = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(i);
+  this._pairs--;
 
   if (bucket) {
-    $.each(bucket, function(index) {
-      if( this[0] === k ) {
+    each(bucket, function(item, index) {
+      if( item[0] === k ) {
         bucket.splice(index, 1);
       }
     });
   }
+
   if(this._pairs < this._limit * 0.25) {
     this.reHash(this._limit / 2);
   }
@@ -59,6 +68,7 @@ HashTable.prototype.remove = function(k){
 
 HashTable.prototype.reHash = function(newLimit) { 
   var temp = [];
+  
   this._storage.each(function(bucket) {
     if(bucket) {
       each(bucket, function(item){
@@ -71,12 +81,10 @@ HashTable.prototype.reHash = function(newLimit) {
   this._storage = makeLimitedArray(this._limit);
   this._pairs = 0;
 
-  // for (var i = 0; i < temp.length; i++) {
-  //   this.insert(temp[i][0], temp[i][1]);
-  // }
-  // each(temp, function(item) {
-  //   this.insert(item[0], item[1]);
-  // });
+  var hash = this;
+  each(temp, function(item) {
+    hash.insert(item[0], item[1]);
+  });
 };
 
 var each = function(collection, iterator) {
