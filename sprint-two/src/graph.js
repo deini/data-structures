2d4e601602b0d668f41797c57b97e8cb3432bc19
chughes87 <jeanette.pettibone@gmail.com>
2014-01-11 16:41:04 -0800

    Implemented naive addNode and contains

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,10 +1,14 @@
 var Graph = function(){
+  this.nodes = {};
+  this.edges = {};
 };
 
 Graph.prototype.addNode = function(newNode, toNode){
+  this.nodes[newNode] = newNode;
 };
 
 Graph.prototype.contains = function(node){
+  return this.nodes[node] ? true : false; 
 };
 
 Graph.prototype.removeNode = function(node){
