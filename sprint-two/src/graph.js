0d8556fbda852f093960ac19cba1ad16f1a573bb
chughes87 <jeanette.pettibone@gmail.com>
2014-01-11 18:45:24 -0800

    implemented automatically adding edge between first and second node in graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -4,6 +4,11 @@ var Graph = function(){
 };
 
 Graph.prototype.addNode = function(newNode, toNode){
+  var keys = Object.keys(this.nodes);
+
+  if (keys.length === 1) {
+    this.addEdge(this.nodes[keys[0]], newNode);
+  }
   this.nodes[newNode] = newNode;
 };
 
@@ -16,9 +21,17 @@ Graph.prototype.removeNode = function(node){
 };
 
 Graph.prototype.getEdge = function(fromNode, toNode){
+  if (this.edges[fromNode][toNode]) {
+    return true;
+  }
+  return false;
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  if (!this.edges[fromNode]) {
+    this.edges[fromNode] = {};
+  }
+  this.edges[fromNode][toNode] = true;
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
