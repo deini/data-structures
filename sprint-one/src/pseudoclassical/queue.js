8dcb0112c265e411efafac0c68e70478cf0e0362
Daniel Almaguer <danielalmaguer@gmail.com>
2014-01-12 23:31:52 -0800

    Added LinkedList implementation to the queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,21 +1,47 @@
 var Queue = function(){
-  this.storage = makeLinkedList();
-  this.thesize = 0;
+  this._storage = new LinkedList();
+  this._size = 0;
 };
 
 Queue.prototype.enqueue = function(value) {
-  this.storage.addToTail(value);
-  this.thesize++;
+  this._size++;
+  this._storage.addToTail(value);
 };
 
 Queue.prototype.dequeue = function() {
-  this.thesize = this.thesize && --this.thesize;
-  return this.storage.removeHead();
+  this._size = this._size && --this._size;
+  return this._storage.removeHead();
 };
 
 Queue.prototype.size = function() {
-  return this.thesize;
+  return this._size;
 };
 
-var MyQueue = new Queue();
+var Node = function(value) {
+  this.value = value;
+  this.next = null;
+};
 
+var LinkedList = function() {
+  this._head = null;
+  this._tail = null;
+};
+
+LinkedList.prototype.removeHead = function(){
+  if(this._head) {
+    var tempHead = this._head;
+    this._head = this._head.next;
+    return tempHead.value;
+  }
+  return null;
+};
+
+LinkedList.prototype.addToTail = function(value) {
+  if(this._head) {
+    this._tail.next = new Node(value);
+    this._tail = this._tail.next;
+  } 
+  else {
+    this._head = this._tail = new Node(value);
+  }
+};
